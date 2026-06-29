1a9be410ad90f4a74d77ecaca67a2b3817c580c6
Eleni-D <elenidemlie45@gmail.com>
2026-06-29 17:45:29 +0300

    Updated transform.py with new modifications

--- transform.py
+++ transform.py
@@ -1,1 +1,15 @@
+from OpenGL.GL import *
 
+
+def apply_shape_transformations(shape):
+    center = shape.get_center()
+
+    glTranslatef(shape.translate_x, shape.translate_y, 0)
+
+    glTranslatef(center.x, center.y, 0)
+
+    glRotatef(shape.rotation_angle, 0, 0, 1)
+
+    glScalef(shape.scale_x, shape.scale_y, 1)
+
+    glTranslatef(-center.x, -center.y, 0)
